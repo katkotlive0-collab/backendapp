677290cef7e0db5d87baa166cb78986d706c36a2
Abrar Ali <katkotlive0@gmail.com>
2025-12-06 19:37:59 +0500

    update

--- config.js
+++ config.js
@@ -3,18 +3,18 @@ module.exports = {
   PORT: process.env.PORT || 5000,
 
   //secret key for API
-  SECRET_KEY: "E21A7F1EF5563ABBCD24615B2E7FE",
+  SECRET_KEY: process.env.SECRET_KEY || "E21A7F1EF5563ABBCD24615B2E7FE",
 
   //gmail credentials for send email
-  EMAIL: "katkotlive0@gmail.com",
-  PASSWORD: "PASSWORD",
+  EMAIL: process.env.EMAIL || "katkotlive0@gmail.com",
+  PASSWORD: "pvcl azlf yyaq rjcm",
 
   //secret key for jwt
-  JWT_SECRET: "E21A7F1EF5563ABBCD24615B2E7FE",
+  JWT_SECRET: process.env.JWT_SECRET || "E21A7F1EF5563ABBCD24615B2E7FE",
 
   //baseURL
-  baseURL: "https://seahorse-app-dus62.ondigitalocean.app/",
+  baseURL: process.env.BASE_URL || "https://seahorse-app-dus62.ondigitalocean.app/",
 
   //firebase server key for send notification
-  SERVER_KEY: "SERVER_KEY",
+  SERVER_KEY: process.env.SERVER_KEY || "server_key_here",
 };
